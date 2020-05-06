fd550fc1ecfc8c4fcc4f474d261633fe42685637
Anurag101292 <anurag10upadhyay@gmail.com>
2020-05-06 17:00:41 +0530

    Log files add

--- src/test/java/com/CN/Base/TestBase.java
+++ src/test/java/com/CN/Base/TestBase.java
@@ -3,14 +3,19 @@ package com.CN.Base;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+import org.apache.log4j.Logger;;
 
 public class TestBase {
 
@@ -18,13 +23,12 @@ public class TestBase {
 	public static Properties config = new Properties();
 	public static Properties OR = new Properties();
 	public static FileInputStream fis;
+	public static Logger log = Logger.getLogger("devpinoyLogger");
 
 	@BeforeSuite
-	public void setup()
-	{
+	public void setup() {
 
-		if (driver == null)
-		{
+		if (driver == null) {
 
 			try {
 
@@ -37,6 +41,7 @@ public class TestBase {
 			}
 			try {
 				config.load(fis);
+				log.debug("Config file loaded !!!");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -52,31 +57,57 @@ public class TestBase {
 			}
 			try {
 				OR.load(fis);
+				log.debug("OR file loaded !!!");
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 			if (config.getProperty("browser").equalsIgnoreCase("chrome")) {
+				ChromeOptions ops = new ChromeOptions();
+
+				ops.addArguments("--disable-notifications");
+				ops.addArguments("disable-infobars");
+				ops.addArguments("start-maximized");
+				ops.addArguments("--disable-geolocation");
+				ops.addArguments("--enable-strict-powerful-feature-restrictions");
 				System.setProperty("webdriver.chrome.driver",
 						System.getProperty("user.dir") + "/src/test/resources/executables/chromedriver");
-				driver = new ChromeDriver();
+				driver = new ChromeDriver(ops);
+				log.debug("Chrome Launched !!!");
 
 			} else if (config.getProperty("browser").equalsIgnoreCase("firefox")) {
 				System.setProperty("webdriver.chrome.driver",
 						System.getProperty("user.dir") + "/src/test/resources/executables/firefoxdriver");
 				driver = new FirefoxDriver();
+				log.debug("Firefox Launched !!!");
+
 			}
 			driver.get(config.getProperty("URL"));
+			log.debug("Navigated to ==>" + config.getProperty("URL"));
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			
+
 		}
+
 	}
 
-	
+	public boolean isElementPresent(By by) {
+
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException  t) {
+			return false;
+		}
+
+		
+	}
+
 	@AfterSuite
 	public void tearup() {
-		if (driver!=null)
-		{		driver.quit();}
+		if (driver != null) {
+			driver.quit();
+			log.debug("Test Execution completed  !!!");
+		}
 	}
 }
--- src/test/java/com/CN/testcases/Login.java
+++ src/test/java/com/CN/testcases/Login.java
@@ -1,9 +1,25 @@
 package com.CN.testcases;
 
-public class Login {
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
 
-	
-	public void voiduserLogin() 
-	
-	{}
+import com.CN.Base.TestBase;
+
+public class Login extends TestBase {
+
+	@Test
+	public void userLogin()
+
+	{
+		log.debug("Inside Login Test !!!");
+		driver.findElement(By.xpath(OR.getProperty("hotel"))).click();
+		
+		log.debug("Hotel clicked");
+		Assert.assertTrue(isElementPresent(By.xpath(OR.getProperty("searchotel"))),
+				"Succesfully Not Navigate On Hotel Page");
+		log.debug("Succesfully Navigate On Hotel Page");
+	}
 }
