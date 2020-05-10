f57f4c3171bda05c015f2335182697ec9e0a3899
Anurag101292 <anurag10upadhyay@gmail.com>
2020-05-10 14:04:05 +0530

    Screenshot updated working

--- src/test/java/com/CN/listeners/CustomListeners.java
+++ src/test/java/com/CN/listeners/CustomListeners.java
@@ -2,13 +2,18 @@ package com.CN.listeners;
 
 import java.io.IOException;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.events.WebDriverEventListener;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import org.testng.Reporter;
 
 import com.CN.utilities.Screenshot;
 
-public class CustomListeners implements ITestListener {
+public class CustomListeners implements ITestListener ,WebDriverEventListener{
 
 	public void onTestStart(ITestResult result) {
 		// TODO Auto-generated method stub
@@ -17,24 +22,36 @@ public class CustomListeners implements ITestListener {
 
 	public void onTestSuccess(ITestResult result) {
 		// TODO Auto-generated method stub
-		try {
-			Screenshot.captureScreenshot(result.getMethod().getMethodName());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.setProperty("org.uncommons.reportng.escape-output","false");
+		   try {
+				Screenshot.captureScreenshot(result.getMethod().getMethodName());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Screenshot captured");
+			}
+				
+		Reporter.log("Test script passed ", true );
+		Reporter.log("capturing screenshot");
+		
+	 
+		
 		
 	}
 
 	public void onTestFailure(ITestResult result) {
 		// TODO Auto-generated method stub
-		
-		try {
-			Screenshot.captureScreenshot(result.getMethod().getMethodName());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.setProperty("org.uncommons.reportng.escape-output","false");
+		   try {
+				Screenshot.captureScreenshot(result.getMethod().getMethodName());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Screenshotnot  captured");
+			}
+				
+		Reporter.log("Test script failed ", true );
+		Reporter.log("capturing screenshot",true);
 		
 	}
 
@@ -58,6 +75,132 @@ public class CustomListeners implements ITestListener {
 	}
 
 	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeAlertAccept(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterAlertAccept(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterAlertDismiss(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeAlertDismiss(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateTo(String url, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateTo(String url, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateBack(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateBack(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateForward(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateForward(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateRefresh(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateRefresh(WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterFindBy(By by, WebElement element, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeClickOn(WebElement element, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterClickOn(WebElement element, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeScript(String script, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterScript(String script, WebDriver driver) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onException(Throwable throwable, WebDriver driver) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/test/java/com/CN/utilities/Screenshot.java
+++ src/test/java/com/CN/utilities/Screenshot.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.Reporter;
 
 import com.CN.Base.TestBase;
 
@@ -18,7 +19,7 @@ public class Screenshot extends TestBase {
 
 	public static void captureScreenshot(String testcasename) throws IOException {
 
-		String imgpath = System.getProperty("user.dir").concat("\\screenshot\\" + testcasename);
+		String imgpath = System.getProperty("user.dir").concat("/screenshot/" + testcasename);
 
 		File f = new File(imgpath);
 		if (!f.exists()) {
@@ -28,11 +29,12 @@ public class Screenshot extends TestBase {
 		Date d = new Date();
 		SimpleDateFormat sd = new SimpleDateFormat("dd_MM_yy_HH_mm_ss_a");
 		String timestamp = sd.format(d);
-		destpath = imgpath + "\\" + timestamp + ".png";
+		destpath = imgpath + "/" + timestamp + ".png";
 		File dest = new File(destpath);
 		TakesScreenshot t = (TakesScreenshot) driver;
 		File srcfile = t.getScreenshotAs(OutputType.FILE);
 		FileUtils.copyFile(srcfile, dest);
+		Reporter.log("<a target=\"_blank\" href="+destpath+">Screenshot</a>",true);
 
 	}
 }
