a3fefffda5039da32d2cce59e4a6fbd6ace6beaf
anurag101292 <anurag10upadhyay@gmail.com>
2022-04-11 03:09:16 +0530

    WiremockFunctionality

--- src/test/java/com/CN/FunctionalProgramming/Streams/AVGOFSTREAM.java
+++ src/test/java/com/CN/FunctionalProgramming/Streams/AVGOFSTREAM.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class AVGOFSTREAM {
     public static void main(String[] args) {
-        int arr[] ={8,7,6,3,8,7,6,3,6,3,8,6,8,7,3,6,9,8,3};
-     double avg=   Arrays.stream(arr).average().getAsDouble();
+        int arr[] = {8, 7, 6, 3, 8, 7, 6, 3, 6, 3, 8, 6, 8, 7, 3, 6, 9, 8, 3};
+        double avg = Arrays.stream(arr).average().getAsDouble();
         System.out.println(avg);
     }
 }
