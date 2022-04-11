15ba990f5ff27484e8f780e6b4461a611a495e3b
anurag101292 <anurag10upadhyay@gmail.com>
2022-04-12 00:58:24 +0530

    Manual Testing FUNCTIONAL PROGRAMMING

---
+++ src/test/java/com/CN/utilities/READDataFROMTEXTFILE.java
@@ -0,0 +1,19 @@
+package com.CN.utilities;
+import java.io.*;
+
+public class READDataFROMTEXTFILE {
+
+        public static void main(String[] args) throws Exception
+        {
+            // Passing the path to the file as a parameter
+            FileReader fr = new FileReader(
+                    "C:\\Users\\pankaj\\Desktop\\test.txt");
+            // Declaring loop variable
+            int i;
+            // Holds true till there is nothing to read
+            while ((i = fr.read()) != -1)
+
+                // Print all the content of a file
+                System.out.print((char)i);
+        }
+    }
